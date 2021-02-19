a419608e48cc9ab9b6fb0b2ea14bae72c89ea8ff
Boris <lopez98boris@gmail.com>
2021-02-18 22:46:31 -0400

    Update

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
-import { MaterialModule } from '../material/material/material.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
