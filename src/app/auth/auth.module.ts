f70ef8443fdef7a95488d91ebc5e99820f05f9ff
Boris <lopez98boris@gmail.com>
2021-02-21 21:38:22 -0400

    Add Auth Dialog

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,14 +7,17 @@ import { MaterialModule } from '../material/material.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RegisterOfficeComponent } from './components/register-office/register-office.component';
+import { AuthDialogComponent } from './shared/auth-dialog/auth-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, RegisterOfficeComponent],
+  declarations: [LoginComponent, RegisterComponent, RegisterOfficeComponent, AuthDialogComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { AuthDialogComponent } from '../../shared/auth-dialog/auth-dialog.component';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form : FormGroup;
+  constructor(public dialog: MatDialog, private fb: FormBuilder) {
+    this.buildForm();
   }
-  register(){
-    console.log('d')
+
+  ngOnInit(): void {}
+  private buildForm() {
+    this.form = this.fb.group({
+      fullname: ['', Validators.required]
+    })
   }
+  register(event) {
+    const dialogRef = this.dialog.open(AuthDialogComponent, {
+      width: '462px',
+      data: {content:'user'}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+ 
+    });
+  }
+
 
 }
